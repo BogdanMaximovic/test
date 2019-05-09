1a1aebe3530ced8a9c471e9001efdf35cf19e8e0
Bogdan <42995409+BogdanMaximovic@users.noreply.github.com>
2019-05-09 13:08:42 +0200

    Add files via upload

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,17 @@
-$(document).ready(function($) {
-	$('.nav-container-content a').click(function() {
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top - 10
-        }, 1200);
-        return false;
-    });
-
+$(function($) {
 	$('.header-nav').on('click', function(event) {
 		event.preventDefault();
 		$('.nav-container-content').toggle();
-	});
-
+		$('.main').on('click', function(event) {
+			event.preventDefault();
+			$('.nav-container-content').hide()
+		});
+		$('.nav-container-content a').click(function() {
+	        $('.nav-container-content').hide('slow')
+	        $('html, body').animate({
+	            scrollTop: $($.attr(this, 'href')).offset().top - 0
+	        }, 800)
+	        return false;
+	    });
+	})
 });
